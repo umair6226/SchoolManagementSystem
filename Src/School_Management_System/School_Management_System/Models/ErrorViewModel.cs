8c4fca2550bdbf9616eebbb59c1cc273b3cee009
Umair Gul <115822650+umair6226@users.noreply.github.com>
2023-01-06 21:49:37 +0500

    School Project

---
+++ Src/School_Management_System/School_Management_System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace School_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
